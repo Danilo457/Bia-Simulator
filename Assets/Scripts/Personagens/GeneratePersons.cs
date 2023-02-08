595a0a40357eed6da6e7818542c63059f05b4cd0
Danilo Pereira <67422689+Danilo457@users.noreply.github.com>
2023-02-07 23:17:09 -0200

    2023-02-07
    Comecei a ver uma Forma de Spam de NPCs, só ta uns Script vou mexer amanhã.

---
+++ Assets/Scripts/Personagens/GeneratePersons.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneratePersons : MonoBehaviour
+{
+    //[SerializeField] 
+
+    void Start()
+    {
+        /*
+        switch ()
+        {
+            case "Amai Odayaka":
+                Debug.Log("Amai");
+
+                break;
+            case "Luana":
+                Debug.Log("Luana");
+
+                break;
+        }
+        */
+    }
+}
---
+++ Assets/Scripts/ScriptableObejt/ScriptableBancoDeDados.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ScriptableObject", menuName = "Scriptable/Banco de Dados")]
+public class ScriptableBancoDeDados : ScriptableObject
+{
+    
+}
